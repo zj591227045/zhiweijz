f4b832f7803c7d9e7bb5a90617225dd3366a009a
zj591227045 <zj591227045@gmail.com>
2025-06-10 20:09:34 +0800

    前端：修复动态路由页面路由冲突问题

--- apps/web/src/pages/simple-test.tsx
+++
@@ -1,8 +0,0 @@
-export default function SimpleTestPage() {
-  return (
-    <div>
-      <h1>Simple Test Page</h1>
-      <p>This is a simple test page to verify Pages Router works.</p>
-    </div>
-  );
-}
